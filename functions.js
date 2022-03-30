cc43050bebf791d7ce816734fcc8a7765b765188
Mateusz <matkub1992@gmail.com>
2022-03-30 13:36:55 +0200

    Enable prompt, color squares
    Start game with button click, insert number that will
    generate number of squares in power of the number inserted.

--- functions.js
+++ functions.js
@@ -1,46 +1,53 @@
-
-// So you need to create an if statement where each iteration
-// creates another squares. It must be squared - so e.g.
-// [3 * 3 * 3] or [4 * 4 * 4 *4] ,etc.
-// Start with 16 x 16 (16 times).
-
-// Maybe if? if there is 16 iterations, there will be 
-// a new power added to the result and create new divs each time
-
-// weneed to base ourself on <power>, each iteration
-// will give us number of squares <variable> multiplied by
-// the same number
-
-let squareWidth = 16; // narazie, potem x
+let squareWidth; 
 let squareAmount;
 
 function calculateSquares() {
-    
-        for (i = 1; i <= 16; i++) {
+        squareWidth = prompt('Insert a number!', 2); // decides how many grinds there will be from 2 to 100
+        if (squareWidth > 100) {
+            squareWidth = prompt('Apologies, needs to be lower than 100', 2);
+        } else if (squareWidth < 2) {
+            squareWidth = prompt('Apologies, needs to be more than 2', 2);
+        }
+
+        for (i = 1; i <= squareWidth; i++) {
             squareAmount = squareWidth * squareWidth;
         } 
 }
 
-calculateSquares();
-
 function placeSquares() {
     x = (800 / squareWidth) + `px`; // Adjusts the width of each square to the main div
     y = x; // Addjusts the height of each square to the main div
+    let square;
+    const backgroundSquares = document.querySelector('#etch_a_sketch'); 
 
     for (j = 1; j <= squareAmount; j++) {
-        const backgroundSquares = document.querySelector('#etch_a_sketch'); 
-        const square = document.createElement('div');
+        square = document.createElement('div');
         square.classList.add('each_square');
-        square.style.backgroundColor = 'blue';
+        square.style.backgroundColor = 'white';
         square.style.width = x;
         square.style.height = y;
         backgroundSquares.appendChild(square);
     }
-}
 
-placeSquares();
+    const squares = document.querySelectorAll('.each_square'); // selects squares built in the loop 
+    // (it's accessible as we defined the square before the loop)
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', () => {
+          square.style.backgroundColor = 'grey';
+        });
+      });
+    }
+
+function startGame() {
+    const reset = document.querySelector('button');
+      reset.addEventListener ('click', function() {
+          calculateSquares();
+          placeSquares();
+      })
+};
+
+
+startGame();
+
 
 console.log(squareAmount);
-
-// kwadraty musza sie pomniejszac, np 800px bedzie kwadrat glowny wiec jak jest 16 to bedzie
-// 800 / 16 = 50px na kwadrat,
