b8247b83835f1c25148ee0c27af95ea9f337dcd9
Mateusz <matkub1992@gmail.com>
2022-03-30 12:27:16 +0200

    Print squares in the browser
    The squares print in the browser and their height x weight adjust
    to the 800px x 800 px square depending on the number of squares.

--- functions.js
+++ functions.js
@@ -1,6 +1,4 @@
 
-const square = document.createElement('div');
-
 // So you need to create an if statement where each iteration
 // creates another squares. It must be squared - so e.g.
 // [3 * 3 * 3] or [4 * 4 * 4 *4] ,etc.
@@ -14,15 +12,35 @@ const square = document.createElement('div');
 // the same number
 
 let squareWidth = 16; // narazie, potem x
+let squareAmount;
 
-function placeSquares() {
-    let squareAmount;
-
+function calculateSquares() {
+    
         for (i = 1; i <= 16; i++) {
             squareAmount = squareWidth * squareWidth;
-        }
-    
-        console.log(squareAmount);
+        } 
+}
+
+calculateSquares();
+
+function placeSquares() {
+    x = (800 / squareWidth) + `px`; // Adjusts the width of each square to the main div
+    y = x; // Addjusts the height of each square to the main div
+
+    for (j = 1; j <= squareAmount; j++) {
+        const backgroundSquares = document.querySelector('#etch_a_sketch'); 
+        const square = document.createElement('div');
+        square.classList.add('each_square');
+        square.style.backgroundColor = 'blue';
+        square.style.width = x;
+        square.style.height = y;
+        backgroundSquares.appendChild(square);
+    }
 }
 
 placeSquares();
+
+console.log(squareAmount);
+
+// kwadraty musza sie pomniejszac, np 800px bedzie kwadrat glowny wiec jak jest 16 to bedzie
+// 800 / 16 = 50px na kwadrat,
